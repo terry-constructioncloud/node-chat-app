6c6d7df91ec7395980ef35695df0233deda60733
terry <terry@constructioncloud.com.au>
2018-01-12 15:43:03 +1100

    include socket.io

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
     console.log(`app is running on ${port}`);
 });
