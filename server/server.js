6b96cef207cdaca57008ac51c41b0edb242cf2f8
terry <terry@constructioncloud.com.au>
2018-01-12 14:59:26 +1100

    skeleton

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+    console.log('app is running ');
+});
