75b7bcb0ec43b779e8a4e220d96802da87431b0d
terry <terry@constructioncloud.com.au>
2018-01-12 15:01:35 +1100

    add npm script

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 app.use(express.static(publicPath));
 app.listen(3000, () => {
-    console.log('app is running ');
+    console.log(`app is running on ${port}`);
 });
